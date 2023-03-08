00be5d7b2a17afa32f1c6c016a8a30d0b7e46d31
ffras3r <126592878+ffras3r@users.noreply.github.com>
2023-03-08 12:15:25 -0500

    Update GLMS.R

--- GLMS.R
+++ GLMS.R
@@ -10,6 +10,9 @@ Data3 <- Data2[Data2$IUCN %in% names(which(table(Data2$IUCN)>5)),]
 Data4 <- Data3[Data3$expectedGen %in% names(which(table(Data3$expectedGen)>5)),]
 redData <- Data4[Data4$realGen %in% names(which(table(Data4$realGen)>5)),]
 
+noMissing <- subset(Data, Data$Locality != "Missing form")
+forIucn <- subset(Data, Data$IUCN != "NR" & Data$IUCN != "NA")
+
 #MISLABELLING PROPORTIONS 
 #Change this var \/ depending on what you want to check
 Table <- table(City, isMislabelled)
@@ -17,7 +20,7 @@ prop_successes <- Table[, 2] / (Table[, 1] + Table[, 2])
 prop_samples <- (Table[, 2] + Table[, 1]) / sum(Table)
 mosaic <- data.frame(Labeled = Table[, 1], Mislabelled = Table[, 2])
 results <- data.frame(Labeled = Table[, 1], Mislabelled = Table[, 2], MislabelledProp = prop_successes, SampleProp = prop_samples)
-results
+# results
 
 #TRAIN THE FOLLOWING GLMS THEN TEST THEM
 set.seed(123)
@@ -70,44 +73,44 @@ mixedR <- glm(isMislabelled ~ isMixed, family = 'binomial', data=redData)
 rawR <- glm(isMislabelled ~ isRaw, family = 'binomial', data=redData)
 
 #AIC AND BIC OF EACH UNIVARIATE GLM
-AIC(cityR)
-BIC(cityR)
-
-AIC(source)
-BIC(source)
-
-AIC(local)
-BIC(local)
-
-AIC(expected)
-BIC(expected)
-
-AIC(iucn)
-BIC(iucn)
-
-AIC(real)
-BIC(real)
-
-AIC(price)
-BIC(price)
-
-AIC(month)
-BIC(month)
-
-AIC(season)
-BIC(season)
-
-AIC(cut)
-BIC(cut)
-
-AIC(mixed)
-BIC(mixed)
-
-AIC(raw)
-BIC(raw)
+# AIC(cityR)
+# BIC(cityR)
+# 
+# AIC(source)
+# BIC(source)
+# 
+# AIC(local)
+# BIC(local)
+# 
+# AIC(expected)
+# BIC(expected)
+# 
+# AIC(iucn)
+# BIC(iucn)
+# 
+# AIC(real)
+# BIC(real)
+# 
+# AIC(price)
+# BIC(price)
+# 
+# AIC(month)
+# BIC(month)
+# 
+# AIC(season)
+# BIC(season)
+# 
+# AIC(cut)
+# BIC(cut)
+# 
+# AIC(mixed)
+# BIC(mixed)
+# 
+# AIC(raw)
+# BIC(raw)
 
 #PREDICTION RESULTS
-#change this variable   \/ to one of the glms above to replicate
+#change this variable         \/ to one of the glms above to replicate
 predictions <- predict(cityR, newdata = test, type = 'response')
 predictions <- ifelse(predictions > 0.5, 1, 0)
 true_pos <- sum(predictions ==1 & isMislabelled == 1)
@@ -119,9 +122,37 @@ accuracy <- (true_pos + true_neg) / (true_neg + true_pos + false_neg + false_pos
 sensitivity <- true_pos / (true_pos + false_neg)
 specificity <- true_neg / (true_neg + false_pos)
 
-accuracy
-sensitivity
-specificity
+# accuracy
+# sensitivity
+# specificity
+
+library(nnet)
+library(MASS)
+priceCity <- multinom(City ~ thePrice, data=Data)
+priceMonth <- multinom(theMonth ~ thePrice, data=Data)
+priceLoc <- multinom(Locality ~ thePrice, data=noMissing)
+priceReal <- multinom(realGen ~ thePrice, data=Data)
+priceExp <- multinom(expectedGen ~ thePrice, data=Data)
+priceRaw <- glm(isRest ~ thePrice, data=noMissing)
+priceCut <- glm(isCut ~ thePrice, data=Data)
+priceMixed <- glm(isMixed ~ thePrice, data=Data)
+#priceIUCN <- polr(IUCN ~ thePrice, data=forIucn, method='logistic')
+
+# summary(priceCity)
+# summary(priceMonth)
+# summary(priceLoc)
+# summary(priceReal)
+# summary(priceExp)
+# (1 - pnorm(abs(summary(priceCity)$coefficients/summary(priceCity)$standard.errors), 0, 1))*2
+# (1 - pnorm(abs(summary(priceMonth)$coefficients/summary(priceMonth)$standard.errors), 0, 1))*2
+# (1 - pnorm(abs(summary(priceLoc)$coefficients/summary(priceLoc)$standard.errors), 0, 1))*2
+# (1 - pnorm(abs(summary(priceReal)$coefficients/summary(priceReal)$standard.errors), 0, 1))*2
+# (1 - pnorm(abs(summary(priceExp)$coefficients/summary(priceExp)$standard.errors), 0, 1))*2
+# 
+# summary(priceRaw)
+# summary(priceCut)
+# summary(priceMixed)
+# summary(priceIUCN)
 
 
 #SUMMARIES
