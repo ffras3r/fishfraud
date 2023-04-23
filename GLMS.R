a177e7af259ba9918e5c529f79859fa636817308
ffras3r <126592878+ffras3r@users.noreply.github.com>
2023-04-22 21:39:41 -0400

    Update GLMS.R

--- GLMS.R
+++ GLMS.R
@@ -1,166 +1,98 @@
-#Read data, from Bob's lab or Oceana
+#Read data, from Bob's lab
 setwd("C:\\Users\\ffras\\OneDrive\\Desktop\\School\\self")
-Data<-read.csv('CollatedDataFinal.csv')
-getwd()
+DataFull<-read.csv('forGit\\CollatedDataFinal.csv')
+Data <- subset(DataFull, DataFull$Sample.source != "Missing form"& DataFull$Sample2 != "Raw/cooked" & DataFull$Sample3 != "rolls/cuts")
+
+# THIS R CODE GOES THROUGH THE FINAL WORKTHOUGH OF BUILDING FREQUENTIST MODELS
+# EACH SECTION OF CODE IS COMMENTED OUT SO PARTS CAN BE UNCOMMENTED FOR OUTPUT READIBILITY
+
+#Remove data with low counts, useful?
+# Data2 <- Data[Data$City %in% names(which(table(Data$City)>5)),]
+# Data3 <- Data2[Data2$IUCN %in% names(which(table(Data2$IUCN)>5)),]
+# Data <- Data3[Data3$expectedGen %in% names(which(table(Data3$expectedGen)>5)),]
+# redData <- Data4[Data4$realGen %in% names(which(table(Data4$realGen)>5)),]
+attach(Data)
 
 
-#Remove data with low counts, 
-Data2 <- Data[Data$City %in% names(which(table(Data$City)>5)),]
-Data3 <- Data2[Data2$IUCN %in% names(which(table(Data2$IUCN)>5)),]
-Data4 <- Data3[Data3$expectedGen %in% names(which(table(Data3$expectedGen)>5)),]
-redData <- Data4[Data4$realGen %in% names(which(table(Data4$realGen)>5)),]
-
-noMissing <- subset(Data, Data$Locality != "Missing form")
-forIucn <- subset(Data, Data$IUCN != "NR" & Data$IUCN != "NA")
-
-#MISLABELLING PROPORTIONS 
+# #MISLABELLING PROPORTIONS 
 #Change this var \/ depending on what you want to check
-Table <- table(City, isMislabelled)
-prop_successes <- Table[, 2] / (Table[, 1] + Table[, 2])
-prop_samples <- (Table[, 2] + Table[, 1]) / sum(Table)
-mosaic <- data.frame(Labeled = Table[, 1], Mislabelled = Table[, 2])
-results <- data.frame(Labeled = Table[, 1], Mislabelled = Table[, 2], MislabelledProp = prop_successes, SampleProp = prop_samples)
+# Table <- table(theMonth, isMislabelled)
+# prop_successes <- Table[, 2] / (Table[, 1] + Table[, 2])
+# prop_samples <- (Table[, 2] + Table[, 1]) / sum(Table)
+# prop_SE <- sqrt((prop_successes * (1-prop_successes)) / (Table[, 1] + Table[, 2])) 
+# mosaic <- data.frame(Labeled = Table[, 1], Mislabelled = Table[, 2]) #for mosaic plots
+# results <- data.frame(Labeled = Table[, 1], Mislabelled = Table[, 2], MislabelledProp = prop_successes, SE = prop_SE, CIlb = (prop_successes-(1.96*prop_SE)), CIub = (prop_successes+(1.96*prop_SE)), SampleProp = prop_samples)
+# TotalMislableProp = sum(Table[, 2]) / (sum(Table[, 1]) + sum(Table[, 2]))
 # results
+# TotalMislableProp
+# 
+# chitest <- data.frame(Labeled = Table[, 1], Mislabelled = Table[, 2])
+# fisher.test(chitest[,1:2], simulate.p.value=TRUE)  #fishers test for a category
 
-#TRAIN THE FOLLOWING GLMS THEN TEST THEM
-set.seed(123)
-traini <- sample(nrow(Data), nrow(Data)*.7)
-train <- Data[traini, ]
-test <- Data[-traini, ]
-#test <- subset(test, IUCN !="EN")
+#TEST OF TWO PROPORTIONS FOR EACH VARIABLE CATEGORY
+# tests <- matrix(nrow=nrow(results), ncol=nrow(results));
+# for(i in 1:nrow(results)){
+#   for(j in 1:nrow(results)){
+#     if(i < j){
+#       p <- (Table[i, 2] + Table[j, 2]) / (Table[i, 1] + Table[j, 1] + Table[i, 2] + Table[j, 2])
+#       tests[i, j] <- pnorm((prop_successes[i] - prop_successes[j]) / (sqrt((p*(1-p)) * (1/(Table[i,1]+Table[i,2]) + 1/(Table[j,1]+Table[j,2])))))
+#     } else{
+#       tests[i,j] <- 0
+#     }
+#   }
+# }
+# tests
 
 #UNIVARIATE GLMS
-#for accuracy / sensitivity / specificity
-# cityR <- glm(isMislabelled ~ City, family = 'binomial', data=train)
-# source <- glm(isMislabelled ~ Sample.source, family = 'binomial', data=train)
-# local <- glm(isMislabelled ~ Locality, family = 'binomial', data=train)
-# expected <- glm(isMislabelled ~ expectedGen, family = 'binomial', data=train)
-# iucn <- glm(isMislabelled ~ IUCN, family = 'binomial', data=train)
-# real <- glm(isMislabelled ~ realGen, family = 'binomial', data=train)
-# price <- glm(isMislabelled ~ thePrice, family = 'binomial', data=train)
-# month <- glm(isMislabelled ~ theMonth, family = 'binomial', data=train)
-# season <- glm(isMislabelled ~ theSeason, family = 'binomial', data=train)
-# cut <- glm(isMislabelled ~ isCut, family = 'binomial', data=train)
-# mixed <- glm(isMislabelled ~ isMixed, family = 'binomial', data=train)
-# raw <- glm(isMislabelled ~ isRaw, family = 'binomial', data=train)
+# cityR <- glm(isMislabelled ~ City, family = 'binomial', data=Data)
+# source <- glm(isMislabelled ~ Sample.source, family = 'binomial', data=Data)
+# local <- glm(isMislabelled ~ Locality, family = 'binomial', data=Data)
+# expected <- glm(isMislabelled ~ expectedGen, family = 'binomial', data=Data)
+# iucn <- glm(isMislabelled ~ IUCN, family = 'binomial', data=Data)
+# price <- glm(isMislabelled ~ thePrice, family = 'binomial', data=Data)
+# month <- glm(isMislabelled ~ theMonth, family = 'binomial', data=Data)
+# season <- glm(isMislabelled ~ theSeason, family = 'binomial', data=Data)
+# cut <- glm(isMislabelled ~ isCut, family = 'binomial', data=Data)
+# mixed <- glm(isMislabelled ~ isMixed, family = 'binomial', data=Data)
+# raw <- glm(isMislabelled ~ isRaw, family = 'binomial', data=Data)
+# sushiType <- glm(isMislabelled ~ Sample4, family = 'binomial', data=Data)
+# 
+# null <- glm(isMislabelled ~ 1, family='binomial', data=Data)
 
-#for aic bic p-values
-cityR <- glm(isMislabelled ~ City, family = 'binomial', data=Data)
-source <- glm(isMislabelled ~ Sample.source, family = 'binomial', data=Data)
-local <- glm(isMislabelled ~ Locality, family = 'binomial', data=Data)
-expected <- glm(isMislabelled ~ expectedGen, family = 'binomial', data=Data)
-iucn <- glm(isMislabelled ~ IUCN, family = 'binomial', data=Data)
-real <- glm(isMislabelled ~ realGen, family = 'binomial', data=Data)
-price <- glm(isMislabelled ~ thePrice, family = 'binomial', data=Data)
-month <- glm(isMislabelled ~ theMonth, family = 'binomial', data=Data)
-season <- glm(isMislabelled ~ theSeason, family = 'binomial', data=Data)
-cut <- glm(isMislabelled ~ isCut, family = 'binomial', data=Data)
-mixed <- glm(isMislabelled ~ isMixed, family = 'binomial', data=Data)
-raw <- glm(isMislabelled ~ isRaw, family = 'binomial', data=Data)
+#FIRST DROP IN DEVIANCE TESTS
+# anova(cityR, null, test='Chisq')
+# anova(source, null, test='Chisq')
+# anova(local, null, test='Chisq')
+# anova(expected, null, test='Chisq')
+# anova(price, null, test='Chisq')
+# anova(iucn, null, test='Chisq')
+# anova(month, null, test='Chisq')
+# anova(season, null, test='Chisq')
+# anova(cut, null, test='Chisq')
+# anova(mixed, null, test='Chisq')
+# anova(raw, null, test='Chisq')
+# anova(sushiType, null, test='Chisq')
 
-#Reduced Data
-cityRR <- glm(isMislabelled ~ City, family = 'binomial', data=redData)
-sourceR <- glm(isMislabelled ~ Sample.source, family = 'binomial', data=redData)
-localR <- glm(isMislabelled ~ Locality, family = 'binomial', data=redData)
-expectedR <- glm(isMislabelled ~ expectedGen, family = 'binomial', data=redData)
-iucnR <- glm(isMislabelled ~ IUCN, family = 'binomial', data=redData)
-realR <- glm(isMislabelled ~ realGen, family = 'binomial', data=redData)
-priceR <- glm(isMislabelled ~ thePrice, family = 'binomial', data=redData)
-monthR <- glm(isMislabelled ~ theMonth, family = 'binomial', data=redData)
-seasonR <- glm(isMislabelled ~ theSeason, family = 'binomial', data=redData)
-cutR <- glm(isMislabelled ~ isCut, family = 'binomial', data=redData)
-mixedR <- glm(isMislabelled ~ isMixed, family = 'binomial', data=redData)
-rawR <- glm(isMislabelled ~ isRaw, family = 'binomial', data=redData)
-
-#AIC AND BIC OF EACH UNIVARIATE GLM
+# #AIC OF EACH UNIVARIATE GLM
 # AIC(cityR)
-# BIC(cityR)
-# 
 # AIC(source)
-# BIC(source)
-# 
 # AIC(local)
-# BIC(local)
-# 
 # AIC(expected)
-# BIC(expected)
-# 
 # AIC(iucn)
-# BIC(iucn)
-# 
 # AIC(real)
-# BIC(real)
-# 
 # AIC(price)
-# BIC(price)
-# 
 # AIC(month)
-# BIC(month)
-# 
 # AIC(season)
-# BIC(season)
-# 
 # AIC(cut)
-# BIC(cut)
-# 
 # AIC(mixed)
-# BIC(mixed)
-# 
 # AIC(raw)
-# BIC(raw)
+# AIC(sushiType)
 
-#PREDICTION RESULTS
-#change this variable         \/ to one of the glms above to replicate
-predictions <- predict(cityR, newdata = test, type = 'response')
-predictions <- ifelse(predictions > 0.5, 1, 0)
-true_pos <- sum(predictions ==1 & isMislabelled == 1)
-false_pos <- sum(predictions == 1 & isMislabelled == 0)
-true_neg <- sum(predictions == 0 & isMislabelled == 0)
-false_neg <- sum(predictions == 0 & isMislabelled == 1)
-
-accuracy <- (true_pos + true_neg) / (true_neg + true_pos + false_neg + false_pos)
-sensitivity <- true_pos / (true_pos + false_neg)
-specificity <- true_neg / (true_neg + false_pos)
-
-# accuracy
-# sensitivity
-# specificity
-
-library(nnet)
-library(MASS)
-priceCity <- multinom(City ~ thePrice, data=Data)
-priceMonth <- multinom(theMonth ~ thePrice, data=Data)
-priceLoc <- multinom(Locality ~ thePrice, data=noMissing)
-priceReal <- multinom(realGen ~ thePrice, data=Data)
-priceExp <- multinom(expectedGen ~ thePrice, data=Data)
-priceRaw <- glm(isRest ~ thePrice, data=noMissing)
-priceCut <- glm(isCut ~ thePrice, data=Data)
-priceMixed <- glm(isMixed ~ thePrice, data=Data)
-#priceIUCN <- polr(IUCN ~ thePrice, data=forIucn, method='logistic')
-
-# summary(priceCity)
-# summary(priceMonth)
-# summary(priceLoc)
-# summary(priceReal)
-# summary(priceExp)
-# (1 - pnorm(abs(summary(priceCity)$coefficients/summary(priceCity)$standard.errors), 0, 1))*2
-# (1 - pnorm(abs(summary(priceMonth)$coefficients/summary(priceMonth)$standard.errors), 0, 1))*2
-# (1 - pnorm(abs(summary(priceLoc)$coefficients/summary(priceLoc)$standard.errors), 0, 1))*2
-# (1 - pnorm(abs(summary(priceReal)$coefficients/summary(priceReal)$standard.errors), 0, 1))*2
-# (1 - pnorm(abs(summary(priceExp)$coefficients/summary(priceExp)$standard.errors), 0, 1))*2
-# 
-# summary(priceRaw)
-# summary(priceCut)
-# summary(priceMixed)
-# summary(priceIUCN)
-
-
-#SUMMARIES
 # summary(cityR)
 # summary(source)
 # summary(local)
 # summary(expected)
-# summary(real)
+# #summary(real)
 # summary(price)
 # summary(iucn)
 # summary(month)
@@ -168,3 +100,106 @@ priceMixed <- glm(isMixed ~ thePrice, data=Data)
 # summary(cut)
 # summary(mixed)
 # summary(raw)
+
+#TWO VARIABLE MODELS
+# priceCity <- glm(isMislabelled ~ isRest + City, family='binomial', data=Data)
+# priceSource <- glm(isMislabelled ~ isRest + thePrice, family='binomial', data=Data)
+# pricelocal <- glm(isMislabelled ~ isRest + Locality, family='binomial', data=Data)
+# priceiucn <- glm(isMislabelled ~ isRest + IUCN, family='binomial', data=Data)
+# priceMonth <- glm(isMislabelled ~ isRest + theMonth, family='binomial', data=Data)
+# priceSeason <- glm(isMislabelled ~ isRest + theSeason, family='binomial', data=Data)
+# priceCut <- glm(isMislabelled ~ isRest + isCut, family='binomial', data=Data)
+# priceMixed <- glm(isMislabelled ~ isRest + isMixed, family='binomial', data=Data)
+# priceRaw <- glm(isMislabelled ~ isRest + isRaw, family='binomial', data=Data)
+# 
+# AIC(priceCity)
+# AIC(priceSource)
+# AIC(pricelocal)
+# AIC(priceiucn)
+# AIC(priceMonth)
+# AIC(priceSeason)
+# AIC(priceCut)
+# AIC(priceMixed)
+# AIC(priceRaw)
+
+
+# THREE VAIRABLE MODELS
+# priceSourceCity <- glm(isMislabelled ~ thePrice + isRest + City, family='binomial', data=Data)
+# priceSourceLocal <- glm(isMislabelled ~ thePrice + isRest + Locality, family='binomial', data=Data)
+# priceSourceIUCN <- glm(isMislabelled ~ thePrice + isRest + IUCN, family='binomial', data=Data)
+# priceSourceMonth <- glm(isMislabelled ~ thePrice + isRest + theMonth, family='binomial', data=Data)
+# priceSourceSeason <- glm(isMislabelled ~ thePrice + isRest + theSeason, family='binomial', data=Data)
+# priceSourceCut <- glm(isMislabelled ~ thePrice + isRest + isCut, family='binomial', data=Data)
+# priceSourceMixed <- glm(isMislabelled ~ thePrice + isRest + isMixed, family='binomial', data=Data)
+# priceSourceRaw <- glm(isMislabelled ~ thePrice + isRest + isRaw, family='binomial', data=Data)
+# 
+# AIC(priceSourceCity)
+# AIC(priceSourceLocal)
+# AIC(priceSourceIUCN)
+# AIC(priceSourceMonth)
+# AIC(priceSourceSeason)
+# AIC(priceSourceCut)
+# AIC(priceSourceMixed)
+# AIC(priceSourceRaw)
+
+
+#PREDICTION RESULTS, preliminary, didn't get to analysing this kinda cool though
+# 
+# #change this variable         \/ to one of the glms above to replicate
+# final <- glm(isMislabelled ~ expectedGen, data = train, family=binomial)
+# predictions <- predict(final, newdata = test, type = 'response')
+# predictions <- ifelse(predictions > 0.5, 1, 0)
+# true_pos <- sum(predictions ==1 & isMislabelled == 1)
+# false_pos <- sum(predictions == 1 & isMislabelled == 0)
+# true_neg <- sum(predictions == 0 & isMislabelled == 0)
+# false_neg <- sum(predictions == 0 & isMislabelled == 1)
+# 
+# accuracy <- (true_pos + true_neg) / (true_neg + true_pos + false_neg + false_pos)
+# sensitivity <- true_pos / (true_pos + false_neg)
+# specificity <- true_neg / (true_neg + false_pos)
+# 
+# accuracy
+# sensitivity
+# specificity
+
+#PREDICT PRICE FOR MISSING PRICE OBSERVATION
+# one <- lm(thePrice ~ isMixed)
+# two <- lm(thePrice ~ isCut)
+# three <- lm(thePrice ~ City)
+# four <- lm(thePrice ~ theMonth)
+# five <- lm(thePrice ~ expectedGen)
+# six <- lm(thePrice ~ isRest)
+# seven <- lm(thePrice ~ isRaw)
+# 
+# summary(one)
+# summary(two)
+# summary(three)
+# summary(four)
+# summary(five)
+# summary(six)
+# summary(seven)
+# 
+# full <- lm(thePrice ~  isCut + isRest + isRaw)
+# red <- lm(thePrice ~  isCut + isRest)
+# 
+# summary(full)
+# summary(red)
+# 
+# anova(full, red)
+
+#INTERACTION TERMS
+#FULL MODEL
+# model1 <- glm(isMislabelled ~ thePrice + theSeason + isRest, family=binomial, data=Data)
+# int1 <- glm(isMislabelled ~ thePrice + theSeason + isRest + theSeason:isRest, family=binomial, data=Data)
+# int2 <- glm(isMislabelled ~ thePrice + theSeason + isRest + thePrice:isRest, family=binomial, data=Data)
+# int3 <- glm(isMislabelled ~ thePrice + theSeason + isRest + thePrice:theSeason, family=binomial, data=Data)
+# int4 <- glm(isMislabelled ~ thePrice + theSeason + isRest + thePrice:theSeason:isRest, family=binomial, data=Data)
+# anova(int1, model1, test='Chisq')
+# anova(int2, model1, test='Chisq')
+# anova(int3, model1, test='Chisq')
+# anova(int4, model1, test='Chisq')
+# 
+# #OPTIMAL MODEL
+# model2 <- glm(isMislabelled ~ thePrice + isRest, family=binomial, data=Data)
+# int <- glm(isMislabelled ~ thePrice + isRest + thePrice:isRest, family=binomial, data=Data)
+# anova(int, model2, test='Chisq')
